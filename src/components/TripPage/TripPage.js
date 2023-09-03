f50f6ac538bc34c38c0cc3ba904e68328fbb4454
EduardoCilvetiUA <eacilveti@miuandes.cl>
2023-09-03 16:11:40 -0300

    TripPage css y js detalles chicos

--- src/components/TripPage/TripPage.js
+++ src/components/TripPage/TripPage.js
@@ -10,6 +10,7 @@ function TripPage() {
   const { tripId } = useParams();
   const [trip, setTrip] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null); // Agregar estado para manejar errores
 
   useEffect(() => {
     fetch(`http://localhost:3000/api/v1/trips/${tripId}`, {
@@ -18,13 +19,32 @@ function TripPage() {
         Authorization: `Bearer ${authToken}`,
       },
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('No se pudo obtener el viaje');
+        }
+        return response.json();
+      })
       .then((data) => {
         setTrip(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
         setLoading(false);
       });
   }, [tripId, authToken]);
 
+  if (error) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+        <Typography variant="h6" color="error">
+          {error}
+        </Typography>
+      </div>
+    );
+  }
+  
   if (loading) {
     return (
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
@@ -36,30 +56,35 @@ function TripPage() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '100px' }}>
       {trip && (
-        <Card style={{ maxWidth: '400px' }}>
-          {/* Renderizar la imagen del viaje */}
-          <CardMedia
-            component="img"
-            alt={trip.name}
-            height="auto"
-            src={trip.image_url} // Asegúrate de tener la URL de la imagen en tus datos de viaje
-          />
-
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {trip.name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {trip.description}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Fecha de inicio: {trip.start_date}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Fecha de fin: {trip.end_date}
-            </Typography>
-          </CardContent>
-        </Card>
+        <Card className="my-card" sx={{ backgroundColor: '#CFD8DC' }} style={{ maxWidth: '400px' }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {trip.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {trip.description}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Fecha de inicio: {trip.start_date}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Fecha de fin: {trip.end_date}
+          </Typography>
+        </CardContent>
+        
+        <div className="image-container">
+          {/* Renderizar las imágenes del viaje */}
+          {trip.images && trip.images.map((image, index) => (
+            <CardMedia
+              key={index}
+              component="img"
+              alt={trip.name}
+              src={image.url}
+              className="trip-image"
+            />
+          ))}
+        </div>
+      </Card>
       )}
     </div>
   );
