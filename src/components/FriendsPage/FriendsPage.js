923aeec3388139fd609927d4a01e4cae975da776
apps-moviles <apps-moviles@apps.myguest.virtualbox.org>
2023-09-19 12:34:13 -0300

    parte de codigo QR

--- src/components/FriendsPage/FriendsPage.js
+++ src/components/FriendsPage/FriendsPage.js
@@ -1,8 +1,38 @@
 //Create starter component
 import React from 'react';
 import { Box } from "@mui/material";
-
 import SpinnerOfDoom from "../HomePage/SpinnerOfDoom";
+import { useLocation } from "react-router-dom";
+import { useAuth } from '../AuthContext.js';
+
+function FriendshipTokenHandler() {
+    const location = useLocation();
+    const { authToken } = useAuth();
+
+    useEffect(() => {
+        const queryParams = new URLSearchParams(location.search);
+        const friendshipToken = queryParams.get('friendshipToken');
+        
+        if (friendshipToken) {
+            // Enviar el token al backend para crear relación de amistad
+            fetch(`http://localhost:3000/friendship_tokens`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${authToken}`,
+                },
+                body: JSON.stringify({ friendshipToken }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                // Manejar la respuesta del backend si es necesario
+            })
+            .catch(error => {
+                console.error('Error al enviar token al backend:', error);
+            });
+        }
+    }, [location.search]);
+}
 
 function FriendsPage(props) {
     return (
